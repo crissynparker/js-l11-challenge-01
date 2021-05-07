d5212da3aa570d32e5a751f88aeb5e3f3114993c
crissynparker1 <crissynparker@gmail.com>
2021-05-06 19:08:44 -0700

    modified js/scripts.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function () {
+const res = await fetch ()
+const images = await res.json();
+console.log(images);
+};
+
+getImage();
